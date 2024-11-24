8fe50f660a59335b461dc35f488d8df4dd930784
Daniel Phan <danielphan.dptp@gmail.com>
2024-11-24 15:35:22 -0500

    refactoring

--- src/gluon/TestDiscoveryCofig.py
+++
@@ -1,8 +0,0 @@
-class TestDiscoveryConfig:
-    def __init__(self):
-        self.patterns = {
-            "files": ["test_*.py", "*_test.py", "*_tests.py", "tests.py"],
-            "classes": ["Test*", "*Test", "*Tests", "*TestCase"],
-            "functions": ["test_*", "*_test", "should_*", "it_*"],
-            "directories": ["test", "tests", "testing", "unit_tests", "integration_tests"],
-        }
--- tests/example_test.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from gluon.example_main import foo
-
-
-def test_foo():
-    assert foo() == 2
-    pass
